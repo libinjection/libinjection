da6f42f20ea37082add09644c729e82e3d0e5395
Felipe Zipitria <felipe.zipitria@owasp.org>
2024-06-07 08:44:05 -0300

    fix: apply suggestion from code review
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- src/libinjection_sqli.h
+++ src/libinjection_sqli.h
@@ -21,16 +21,11 @@ extern "C" {
 
 enum sqli_flags {
     FLAG_NONE = 0,
-    FLAG_QUOTE_NONE = 1 /* 1 << 0 */
-    ,
-    FLAG_QUOTE_SINGLE = 2 /* 1 << 1 */
-    ,
-    FLAG_QUOTE_DOUBLE = 4 /* 1 << 2 */
-
-    ,
-    FLAG_SQL_ANSI = 8 /* 1 << 3 */
-    ,
-    FLAG_SQL_MYSQL = 16 /* 1 << 4 */
+    FLAG_QUOTE_NONE = 1,   /* 1 << 0 */
+    FLAG_QUOTE_SINGLE = 2, /* 1 << 1 */
+    FLAG_QUOTE_DOUBLE = 4, /* 1 << 2 */
+    FLAG_SQL_ANSI = 8,     /* 1 << 3 */
+    FLAG_SQL_MYSQL = 16    /* 1 << 4 */
 };
 
 enum lookup_type {
